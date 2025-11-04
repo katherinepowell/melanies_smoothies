906d684659bb6cea4690aac7beb9ac85029e1d8f
katherinepowell <katherine.powell@wae.com>
2025-11-04 14:45:14 +0000

    add df

--- streamlit_app.py
+++ streamlit_app.py
@@ -40,6 +40,7 @@ if time_to_insert:
 
 import requests
 smoothiefroot_response = requests.get("https://my.smoothiefroot.com/api/fruit/watermelon")
-st.text(smoothiefroot_response.json())
+#st.text(smoothiefroot_response.json())
+sf_df = st.dataframe(data = smoothiefroot_response.json(), use_containter_width = True)
 
 
